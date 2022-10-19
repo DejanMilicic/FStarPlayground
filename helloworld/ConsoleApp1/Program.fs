a7f0701aa728097e19288f4510f9e0f93185e94c
Dejan Milicic <dejan.milicic@gmail.com>
2022-10-19 12:41:51 +0200

    initial commit of a working example

---
+++ helloworld/ConsoleApp1/Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open TestFSharp
+
+printfn "Hello from F#"
+
+let a = pass1 () () (fun x y -> x = y)
+let b = pass1 5 2 (fun x y -> x = y)
+
+printf "a = %b\n" a
+printf "b = %b" b
